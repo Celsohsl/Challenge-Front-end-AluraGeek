2fd6356c014de382eed741d0d0a9c23738eabbeb
Celsohsl <celsohsl@icloud.com>
2022-05-23 23:23:05 -0300

    criado validações de iniciar sessão

---
+++ assets/js/iniciarSessao.js
@@ -0,0 +1,46 @@
+/* Validando formulario de iniciar sessão
+   ========================================================================== */
+(() => {
+  function verificar() {
+    window.event.preventDefault();
+
+    if (email.value == '') {
+      email.classList.add('input-erro');
+      email.focus();
+      Swal.fire({
+        title: 'O campo email é obrigatório.',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      });
+      return false;
+
+    } else if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1 || (email.value.indexOf('.') - email.value.indexOf('@') == 1)) {
+      email.focus();
+      Swal.fire('Complete o email corretamente.');
+      return false;
+    } else {
+      email.classList.add('input-sucesso');
+    }
+
+    if (senha.value === '') {
+      senha.focus();
+      senha.classList.add('input-erro');
+      Swal.fire({
+        title: 'Digite sua senha.',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      });
+      return false;
+
+    } else if (senha.value.length >= 7) {
+      Swal.fire('Digite no máximo 6 caracteres');
+      return false;
+    } else {
+      senha.classList.add('input-sucesso');
+    }
+
+  }
+
+  const botaologin = document.querySelector('[data-form-button]');
+  botaologin.addEventListener('click', (verificar));
+})();
